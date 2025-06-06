16e1c4f97e9b5c028c8f2101124d31a2b134c694
[hk3635] <[kogurehi@cy-com.co.jp]>
2025-06-06 10:18:49 +0900

    data.sql編集

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,11 @@
-INSERT INTO albums (title, artist, release_date) VALUES
-('Hello World', 'Aurora Beats', '2021-08-20'),
-('Strings of Serenity', 'Harmony Collective', '2020-11-10'),
-('Cosmic Vibes', 'Galaxy Harmonics', '2024-01-18');
-
-INSERT INTO musics (title, duration, album_id) VALUES
+INSERT INTO albums (book_name, user_name, user_id,rental_date,return_id) VALUES
+('コバエ大百科', '佐藤太郎',"A001",'2021-08-20','2021-09-20'),
+('料理のすゝめ', '田中次郎',"B020",'2022-02-16','2022-03-16'),
+('Java入門', '大橋鴨太郎',"C100",'2022-11-03','2022-12-03');
+/*INSERT INTO musics (title, duration, album_id) VALUES
 ('Sunset', '00:03:45', 1),
 ('Moonlight', '00:04:12', 1),
-('Eternal Memories', '00:05:45', 2);
+('Eternal Memories', '00:05:45', 2);*/
 
 --- PW は「track」
-INSERT INTO users (username, password) VALUES ('sample', '$2a$08$hBocxV325GV9RKzHR.lHreJr8DsMrkg6vg0kRTbNnBvUFknu670GC');
+/*INSERT INTO users (username, password) VALUES ('sample', '$2a$08$hBocxV325GV9RKzHR.lHreJr8DsMrkg6vg0kRTbNnBvUFknu670GC');
