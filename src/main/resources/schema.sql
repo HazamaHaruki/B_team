171bb70eaeac2f188db3a2c4b054c99536e42b00
[hk3635] <[kogurehi@cy-com.co.jp]>
2025-06-06 09:52:44 +0900

    SQL編集

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,23 +1,24 @@
 CREATE TABLE albums (
-    album_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    artist VARCHAR(255),
-    release_date DATE,
+    book_name VARCHAR(255) PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL,
+    user_id VARCHAR(255),
+    rental_date DATE,
+    return_id DATE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE musics (
+/*CREATE TABLE musics (
     music_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     duration TIME,
     album_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (album_id) REFERENCES albums(album_id) ON DELETE CASCADE
-);
+);*/
 
-CREATE TABLE users (
+/*CREATE TABLE users (
     user_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(60) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+);*/
